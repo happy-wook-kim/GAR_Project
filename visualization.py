46a578c5c8aaf2a39307dc7db3b059a1a52a4d57
jinwook <catdog6210@gmail.com>
2022-08-02 11:43:37 +0900

    [UPDATE] remove useless print

--- visualization.py
+++ visualization.py
@@ -24,5 +24,4 @@ if __name__ == "__main__":
     list_b = list(range(60, 0, -2))
     list_c = list(range(90, 0, -3))
     list_d = list(range(120, 0, -4))
-    print('d:',len(list_d))
     make_graph(list_a, list_b, list_c, list_d, 'train loss', 'gender validation', 'age validation')
