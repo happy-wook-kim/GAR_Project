ef4476285d514f2033bf34236063d953b8790223
jinwook <catdog6210@gmail.com>
2022-08-02 10:14:33 +0900

    [UPDATE] make three plot graph

--- visualization.py
+++ visualization.py
@@ -6,17 +6,18 @@ import numpy as np
 def make_graph(list_a, list_b, list_c, list_d, label_a, label_b, label_c):
     plt.figure('Training')
     make_plot(list_a, list_b, label_a)
+    print(list_a)
     make_plot(list_a, list_c, label_b)
     make_plot(list_a, list_d, label_c)
     #plt.title('Train visualization graph')
     plt.xlabel('Epoch')
-    #plt.ylabel('')
-    plt.legend(loc='upper left')
+    #plt.ylabel('Cross entropy loss')
+    plt.legend(loc='upper right')
     plt.show()
 
 
 def make_plot(list_a, list_b, label_str):
-    plt.plot(list_a, list_b, ':', label=label_str)
+    plt.plot(list_a, list_b, marker='o', label=label_str)
 
 if __name__ == "__main__":  
     list_a = list(range(0, 30))
